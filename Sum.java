6bc9d9891fbdbd94ce3255bfe73c954da24546fe
Burhanuddin Kader <burhankader5252@gmail.com>
2022-01-26 22:30:12 +0530

    added message in Sum.Java

---
+++ Sum.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Sum {
+    private static void main(String[] args) {
+        int a=5;
+        int b=3;
+        System.out.println("sum is"+(a+b));
+    }
+}
